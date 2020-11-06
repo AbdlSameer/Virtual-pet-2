4c67c18b5ab394c74fbcf970c101d5a60aa79800
Abdul Sameer <71142848+AbdlSameer@users.noreply.github.com>
2020-11-06 15:06:38 +0530

    Update Food.js

--- js/Food.js
+++ js/Food.js
@@ -35,19 +35,17 @@ class food
 
     display()
     {
-        fill(255,255,254);
-    textSize(15);
-    if(lastFed>=12)
-    {
-        text("Last Feed :"+ lastFed%12 + "PM",350,30);
-    }else if(lastFed==0)
-    {
-        text("Last Feed : 12 AM",350,30);
-    }else
-    {
-        text("Last Feed :"+lastFed + "AM", 350, 30);
-    }
-
+        lastfeed =hour()  
+         if(lastFed>=12)
+         { 
+              text("Last Feed : "+ lastFed%12 + " PM", 50,30); 
+         }else if(lastFed==0)
+         { 
+              text("Last Feed : 12 AM",50,30); 
+         }
+         else{
+              text("Last Feed : "+ lastFed + " AM", 50,30);
+         }
 
         var x=80,y=100;
 
